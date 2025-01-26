234c1335395a8b04623d2147bf2cfc4e2f57c64e
Werner Strydom <bloudraak@users.noreply.github.com>
2025-01-26 07:15:58 -0800

    Create main.php

---
+++ main.php
@@ -0,0 +1,8 @@
+#!/usr/bin/env php
+<?php
+# Hello World in PHP
+# PHP scripts start with the opening PHP tag <?php
+# PHP can be embedded in HTML or run as a standalone script
+# Originally designed for web development, PHP now supports CLI applications
+
+echo "Hello, World!\n";
